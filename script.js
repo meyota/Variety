82f3e9d792e55fbcd14541681fe525c7f38460b4
meyota <meyota08@gmail.com>
2023-10-01 14:34:39 +0300

    e-commerce

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const open = document.getElementById("menu");
 const close = document.getElementById("close");
 const nav = document.getElementById("navig");
 
+let lgin = document.getElementById("login-form");
+let regf = document.getElementById("register-form");
+let fbutt = document.getElementById("form-bttn");
+
 if (open) {
   open.addEventListener("click", () => {
     nav.classList.add("active");
@@ -27,3 +31,17 @@ smallImg[2].onclick = function () {
 smallImg[3].onclick = function () {
   BigImage.src = smallImg[3].src;
 };
+
+function register(){
+  lgin.style.left = "-400px";
+  regf.style.left = "50px";
+  fbutt.style.left = "110px";
+}
+
+function login(){
+  lgin.style.left = "50px";
+  regf.style.left = "450px";
+  fbutt.style.left = "0";
+}
+
+
